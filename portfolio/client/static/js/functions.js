3e05d2131afe0af1eeaab119edd35eed2613b16e
William Blackwell <wbprogramming@icloud.com>
2017-07-20 16:55:08 -0500

    more functions

--- portfolio/client/static/js/functions.js
+++ portfolio/client/static/js/functions.js
@@ -4,9 +4,25 @@ $(document).ready(function(){
      breakLines: false,
      lifeLike: true
 	});
+
+
 	$("#scroll").click(function() {
     $('html, body').animate({
         scrollTop: $("#body-two").offset().top
     }, 2000);
-});
-})
+	});
+	
+
+	$("#scroll-nav").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#body-two").offset().top
+    }, 2000);
+	});
+	
+
+	$("#scroll-nav-resume").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#body-three").offset().top
+    }, 2000);
+	});	
+})			
